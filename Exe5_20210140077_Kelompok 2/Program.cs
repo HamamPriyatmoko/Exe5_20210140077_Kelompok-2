7aa78c2bfe6c6ced1fbc52ad09cd534ddcc6da49
Hamam Priyatmoko <hamampriyatmoko81@gmail.com>
2022-12-22 16:50:27 +0700

    membuat fungtion Node

--- Exe5_20210140077_Kelompok 2/Program.cs
+++ Exe5_20210140077_Kelompok 2/Program.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace Exe5_20210140077_Kelompok_2
 {
-    internal class Program
-    {
-        static void Main(string[] args)
+    
+        class Node
         {
+            public string name;
+            public Node next;
         }
-    }
+       
 }
