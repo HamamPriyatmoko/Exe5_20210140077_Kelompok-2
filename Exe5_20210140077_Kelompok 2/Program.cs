ce002b7ede498f743932d8c6a5baedb6292b8247
Hamam Priyatmoko <hamampriyatmoko81@gmail.com>
2022-12-22 15:47:16 +0700

    Add project files.

---
+++ Exe5_20210140077_Kelompok 2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exe5_20210140077_Kelompok_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
