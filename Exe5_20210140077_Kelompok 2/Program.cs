c3899a67b87c370ea14fde93652588b8e344f8a2
Hamam Priyatmoko <hamampriyatmoko81@gmail.com>
2022-12-22 16:59:18 +0700

    membuat method insert

--- Exe5_20210140077_Kelompok 2/Program.cs
+++ Exe5_20210140077_Kelompok 2/Program.cs
@@ -20,7 +20,23 @@ namespace Exe5_20210140077_Kelompok_2
             Hamam = null;
             Priyatmoko = null;
         }
-
+        public void insert()
+        {
+            string k2;
+            Node newnode = new Node();
+            Console.WriteLine("Masukan Element: ");
+            k2 = Console.ReadLine();
+            newnode.name = k2;
+            newnode.next = null;
+            if (Hamam == null)
+            {
+                Hamam = newnode;
+                Priyatmoko = newnode;
+                return;
+            }
+            Hamam = newnode;
+            Priyatmoko = newnode;
+        }
     }
     class Program
     {
